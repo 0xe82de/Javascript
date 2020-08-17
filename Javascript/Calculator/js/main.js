32e6fa0c337b256f25679c65965c469374c356b2
kyoungseokKim <0xe82de@gmail.com>
2020-08-17 18:52:09 +0900

    print function update 2020-08-17 19:00

--- Javascript/Calculator/js/main.js
+++ Javascript/Calculator/js/main.js
@@ -1,3 +1,7 @@
+"use strict";
+
+let checkOp;
+
 function setValue(value) {
   document.getElementById("js-result").innerHTML = value;
 }
@@ -14,4 +18,76 @@ function isNum(value) {
   }
 
   setValue(value);
+}
+
+function isZero() {
+  return document.getElementById("js-result").innerHTML;
+}
+
+function printNum(value) {
+  let checkZero = isZero();
+
+  if (checkZero !== "0" && checkOp === 0) {
+    value = checkZero + value;
+    document.getElementById("js-result").innerHTML = value;
+  } else {
+    document.getElementById("js-result").innerHTML = value;
+  }
+
+  checkOp = 0;
+
+  return;
+}
+
+function printOP(op) {
+  let tmpResult;
+
+  if (document.getElementById("js-tmpResult").innerHTML === "0") {
+    tmpResult = document.getElementById("js-result").innerHTML;
+    document.getElementById("js-tmpResult").innerHTML = tmpResult + " " + op;
+  } else {
+    tmpResult = document.getElementById("js-tmpResult").innerHTML;
+    tmpResult =
+      tmpResult + " " + document.getElementById("js-result").innerHTML;
+    document.getElementById("js-tmpResult").innerHTML = tmpResult + " " + op;
+  }
+
+  return;
+}
+
+function whatKey(key) {
+  if (checkOp === undefined) {
+    checkOp = 0;
+  }
+
+  switch (key) {
+    case "9":
+    case "8":
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+    case "0":
+      // 숫자 ㅋㅋㅋ
+      printNum(key);
+      break;
+    case "/":
+    case "X":
+    case "-":
+    case "+":
+      checkOp = 1;
+      printOP(key);
+      break;
+    case "C":
+      clear();
+      break;
+    case "=":
+      compute();
+      break;
+  }
+
+  return;
 }
